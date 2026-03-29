6259e8f60291487c7766343ec15b2b2714ca945e
Yves-Tabor <walvesoutis.gmail.com>
2026-03-30 01:53:57 +0200

    another one

--- src/raceCondition.jsx
+++ src/raceCondition.jsx
@@ -16,13 +16,31 @@ import React from 'react'
 // - Prevent outdated responses from overwriting newer ones.
 
 function Post({ postId }) {
-const [post, setPost] = React.useState(null);
+  const [post, setPost] = React.useState(null);
 
-React.useEffect(() => {
-    fetch(`https://dummyjson.com/posts/${postId}`)
-    .then(res => res.json())
-    .then(setPost);
-    }, [postId]);
+  React.useEffect(() => {
+    const controller = new AbortController();
 
-return <div>{post?.name}</div>;
+    fetch(`https://dummyjson.com/posts/${postId}`, {
+      signal: controller.signal,
+    })
+      .then(res => res.json())
+      .then(data => setPost(data))
+      .catch(err => {
+        if (err.name !== "AbortError") {
+          console.error(err);
+        }
+      });
+
+    return () => {
+      controller.abort();
+    };
+  }, [postId]);
+
+  return <div>{post?.title}</div>;
 }
+
+// Comments
+// If postId changes quickly, a new fetch is triggerd and finshishes while the old one is yet running and might overwrite the new one in the end.
+// To prevent this, we use AbortController to cancel the previous request.
+// 
