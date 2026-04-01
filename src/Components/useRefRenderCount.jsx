6404e7d345b05e20358ae4d031b13bfb2bf398ae
Yves-Tabor <walvesoutis.gmail.com>
2026-04-01 15:52:32 +0200

    Ongoing exercise

--- src/Components/useRefRenderCount.jsx
+++ src/Components/useRefRenderCount.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 function CountRenders(){
-
-
+    const [render, setRender] = React.useState(0)
+    const renderRef = React.useRef(0)
+    function handleClick(){
+        setRender(prev=> prev + 1)
+    }
     return(
         <>
-        
+        <p>Render Count: {render}</p>
+        <button onClick={handleClick}>Click</button>
         </>
     )
 }
